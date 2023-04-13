d108bbe14c8d5a5560c4ad41002f2d09aab56074
Alex Epik <73494181+EvgeniyPukanovich@users.noreply.github.com>
2023-04-13 12:39:32 +0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,11 +1,13 @@
 import pandas as pd
 import numpy as np
+from scipy import stats
 
-
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 970839957 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array, y: np.array) -> bool:
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    alpha = 0.04
+    p_val = stats.anderson_ksamp([x, y])[2]
+    return True if p_val <= alpha else False # Ваш ответ, True или False
